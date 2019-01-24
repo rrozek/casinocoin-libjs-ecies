d4ba2155fadd15c16b34ea150b8ef34e260e6178
Jakub Rojek <jakubrojek@gmail.com>
2019-01-24 17:09:19 +0100

    test fix

--- test/encrypt_msg.js
+++ test/encrypt_msg.js
@@ -3,35 +3,23 @@ const assert = require('assert');
 const api = require('../');
 const keypairs = require('casinocoin-libjs-keypairs')
 const fixtures = require('./fixtures/api.json');
-
-var resolveEncrypt = function(encryptedMsg) {
-	api.decrypt(fixtures.secp256k1.receiver.master_seed, 
-                Buffer.from(fixtures.secp256k1.sender.master_seed.public_key_hex, 'hex'), 
-                encryptedMsg)
-                .then( function(decryptedMsg) {
-                    return decryptedMsg;
-                });
-}
-
-function encryptDecryptFunc(msgContent) {
-
-};
     
 describe('api', () => {
     it('encrypt-decrypt', async () => {
-        for (var i = 0, len = fixtures.message; i < len; i++) {
+        for (var i = 0, len = fixtures.message.length; i < len; i++) {
+            var msgContent = fixtures.message[i];
             var decryptedMsg = 
-                api.encrypt(
+                await api.encrypt(
                     fixtures.secp256k1.sender.master_seed, 
-                    Buffer.from(fixtures.secp256k1.receiver.master_seed.public_key_hex, 'hex'), 
+                    Buffer.from(fixtures.secp256k1.receiver.public_key_hex, 'hex'), 
                     Buffer.from(msgContent))
                     .then( function(encryptedMsg) {
                         return api.decrypt(
                             fixtures.secp256k1.receiver.master_seed, 
-                            Buffer.from(fixtures.secp256k1.sender.master_seed.public_key_hex, 'hex'), 
+                            Buffer.from(fixtures.secp256k1.sender.public_key_hex, 'hex'), 
                             encryptedMsg);
                     });
-            assert(decryptedMsg === msgContent);
+            assert(decryptedMsg == msgContent);
         }
     });
 });
